ad6fab953429aabd2632b830afc1c69b5af5fa1a
Octavio Barbosa <octaviobar91@gmail.com>
2021-04-03 18:27:32 -0300

    feat: password hash

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+import { hash } from "bcrypt";
+
 @injectable()
 class CreateUserUseCase {
   constructor(
@@ -15,10 +17,18 @@ class CreateUserUseCase {
     password,
     driver_licence,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_licence,
     });
   }
