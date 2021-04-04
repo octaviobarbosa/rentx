1ee9a162e2357e729ca86f6d1198c91672f422fb
Octavio Barbosa <octaviobar91@gmail.com>
2021-04-04 02:07:46 -0300

    feat: custom errors

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationRepository";
 
 interface IRequest {
@@ -19,7 +20,7 @@ class CreateSpecificationUseCase {
     );
 
     if (specificationAlreadyExists) {
-      throw new Error("Specification already excists!");
+      throw new AppError("Specification already exists!");
     }
 
     await this.specificationsRepository.create({ name, description });
